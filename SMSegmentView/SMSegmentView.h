7c487678cad338e00eed5b4cd0aed268f28fd9c4
michallaskowski <michal.laskowski.dev@gmail.com>
2015-10-22 15:24:19 +0200

    Add framework target, useful for Carthage

---
+++ SMSegmentView/SMSegmentView.h
@@ -0,0 +1,19 @@
+//
+//  SMSegmentView.h
+//  SMSegmentView
+//
+//  Created by mlaskowski on 22/10/15.
+//  Copyright © 2015 si.ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMSegmentView.
+FOUNDATION_EXPORT double SMSegmentViewVersionNumber;
+
+//! Project version string for SMSegmentView.
+FOUNDATION_EXPORT const unsigned char SMSegmentViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMSegmentView/PublicHeader.h>
+
+
