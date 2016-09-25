3d0ed978ef10e802e2352ed9d0ff34d74e1b7fdf
Si MA <si.ma@accenture.com>
2016-09-25 21:59:54 +0100

    update sample code

--- SMSegmentViewController/ViewController.swift
+++ SMSegmentViewController/ViewController.swift
@@ -23,9 +23,9 @@ class ViewController: UIViewController {
         
         let appearance = SMSegmentAppearance()
         appearance.segmentOnSelectionColour = UIColor(red: 245.0/255.0, green: 174.0/255.0, blue: 63.0/255.0, alpha: 1.0)
-        appearance.segmentOffSelectionColour = UIColor.whiteColor()
-        appearance.titleOnSelectionFont = UIFont.systemFontOfSize(12.0)
-        appearance.titleOffSelectionFont = UIFont.systemFontOfSize(12.0)
+        appearance.segmentOffSelectionColour = UIColor.white
+        appearance.titleOnSelectionFont = UIFont.systemFont(ofSize: 12.0)
+        appearance.titleOffSelectionFont = UIFont.systemFont(ofSize: 12.0)
         appearance.contentVerticalMargin = 10.0
         
         
@@ -35,10 +35,10 @@ class ViewController: UIViewController {
          */
         let segmentFrame = CGRect(x: self.margin, y: 120.0, width: self.view.frame.size.width - self.margin*2, height: 40.0)
         self.segmentView = SMSegmentView(frame: segmentFrame, dividerColour: UIColor(white: 0.95, alpha: 0.3), dividerWidth: 1.0, segmentAppearance: appearance)
-        self.segmentView.backgroundColor = UIColor.clearColor()
+        self.segmentView.backgroundColor = UIColor.clear
         
         self.segmentView.layer.cornerRadius = 5.0
-        self.segmentView.layer.borderColor = UIColor(white: 0.85, alpha: 1.0).CGColor
+        self.segmentView.layer.borderColor = UIColor(white: 0.85, alpha: 1.0).cgColor
         self.segmentView.layer.borderWidth = 1.0
 
         // Add segments
@@ -46,7 +46,7 @@ class ViewController: UIViewController {
         self.segmentView.addSegmentWithTitle("Blub", onSelectionImage: UIImage(named: "bulb_light"), offSelectionImage: UIImage(named: "bulb"))
         self.segmentView.addSegmentWithTitle("Cloud", onSelectionImage: UIImage(named: "cloud_light"), offSelectionImage: UIImage(named: "cloud"))
         
-        self.segmentView.addTarget(self, action: #selector(ViewController.selectSegmentInSegmentView(_:)), forControlEvents: .ValueChanged)
+        self.segmentView.addTarget(self, action: #selector(selectSegmentInSegmentView(segmentView:)), for: .valueChanged)
         
         // Set segment with index 0 as selected by default
         self.segmentView.selectedSegmentIndex = 0
@@ -61,17 +61,13 @@ class ViewController: UIViewController {
         print("Select segment at index: \(segmentView.selectedSegmentIndex)")
     }
     
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        return UIInterfaceOrientationMask.All
-    }
-    
-    override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+    override func willAnimateRotation(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
         /*
         MARK: Replace the following line to your own frame setting for segmentView.
         */
-        if toInterfaceOrientation == UIInterfaceOrientation.LandscapeLeft || toInterfaceOrientation == UIInterfaceOrientation.LandscapeRight {
+        if toInterfaceOrientation == UIInterfaceOrientation.landscapeLeft || toInterfaceOrientation == UIInterfaceOrientation.landscapeRight {
             
-            self.segmentView.organiseMode = .Vertical
+            self.segmentView.organiseMode = .vertical
             if let appearance = self.segmentView.segmentAppearance {
                 appearance.contentVerticalMargin = 25.0
             }
@@ -79,7 +75,7 @@ class ViewController: UIViewController {
         }
         else {
             
-            self.segmentView.organiseMode = .Horizontal
+            self.segmentView.organiseMode = .horizontal
             if let appearance = self.segmentView.segmentAppearance {
                 appearance.contentVerticalMargin = 10.0
             }
